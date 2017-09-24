7dc43d1705dcc153f9f4b43d231bdb6363ff1bca
irook <koorikun@foxmail.com>
2017-09-24 20:15:16 +0800

    mmp
    mmp

--- analyze.py
+++ analyze.py
@@ -1,17 +1,12 @@
 #解析问卷模块
 
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import  expected_conditions as EC
-
 import time
 
 headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"}
 main_url = "https://wj.qq.com/stat_recycle.html?id=1491015"
 driver = webdriver.Safari()
+driver.set_page_load_timeout(10)
 
 def login():
     #获取界面
@@ -21,25 +16,33 @@ def login():
     #进入登入界面
     qqLoginImg = driver.find_element_by_id("loginTabQQ")
     qqLoginImg.click()
-    time.sleep(6)
+    time.sleep(2)
 
     #寻找所在QQ
-    accountImg = driver.find_element_by_xpath('//*[@id="img_out_"]')
+    driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="QQLoginIframe"]'))
+    accountImg = driver.find_element_by_class_name("img_out")
     accountImg.click()
+    time.sleep(4)
+
+    #保存Cookie到会话中
+    cookies = driver.get_cookies()
+    for eachcookie in cookies:
+        driver.add_cookie(eachcookie)
+    time.sleep(2)
+
+    print("[Log]Login successfully!")
 
 
 def getNo():
     driver.get(main_url)
-    bs = BeautifulSoup(driver.page_source, "lxml")
-
-    list = bs.find("tbody",{"id":"list"}).tr.findAll("td",limit=3)
-    print("The Lastest ID: " + list[2])
-    return list[2]
+    time.sleep(2)
+    no = driver.find_element_by_xpath('//*[@id="list"]/tr[1]/td[3]')
+    print("The latest questionnaire number is: ", no)
+    return no
 
 
 def ana(ID):
     url = "https://wj.qq.com/stat_recycle_answer.html?id=1491015#p1&" + ID
-
     driver.get(url)
 
     return driver.page_source
--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from TencentQuestionnaireReminder import sendmail as mail
 
 last_no = "0"
 
+funcs.login()
+
 while(True):
-    funcs.login()
+    print("----开始搜索网页----")
     now_no = funcs.getNo()
     if now_no is not last_no:
-        mail.send()(funcs.ana())
+        mail.send(funcs.ana(now_no))
+        print("------搜索结束------")
         time.sleep(60)
+    else:
+        print("-----没有新问卷-----")
